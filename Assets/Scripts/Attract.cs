ac7b3809ce731d5f83bac35d4af393d62c0b24d6
Ian-PC\Ian <funnyguy150@gmail.com>
2018-10-03 18:23:21 -0400

    Raycasting enemy FOV.

--- Assets/Scripts/Attract.cs
+++ Assets/Scripts/Attract.cs
@@ -2,34 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attract : MonoBehaviour {
+public class Attract : MonoBehaviour
+{
 
     bool batting;
+    [SerializeField] GameObject anchor;
 
     private Rigidbody body;
     public float speed = 1f;
     public Vector3 currPosition;
+    public LayerMask myMask;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
 
     }
 
 
     // Update is called once per frame
-    void Update ()
-    { 
+    void Update()
+    {
         if (PlayerMovement.batting == true)
         {
-            //move enemy towards player
+            /*//move enemy towards player
             currPosition = transform.position;
             moveEnemy();
-        }		
-	}
+
+            if (Physics2D.Raycast(this.transform.position, anchor.transform.position - this.transform.position, 1000f, myMask))
+            {
+                RaycastHit2D temp = Physics2D.Raycast(this.transform.position, anchor.transform.position - this.transform.position, 1000f, myMask);
+                Debug.Log(temp.transform.gameObject);
+                Debug.Log(temp.transform.tag);
+                if (temp.transform.tag == "Enemy")
+                {   //Call funct
+                    Debug.Log("Got here");
+                    if (temp.transform.GetComponent<EnemyController>())
+                    {
+                        temp.transform.GetComponent<EnemyController>().getCharmed(this.transform.position);
+                    }
+                }
+
+            }*/
+            CanSeePlayer();
+
+            
+        }
+    }
 
     void moveEnemy()
     {
         transform.position = Vector3.MoveTowards(currPosition, PlayerMovement.playerPosition, speed);
+    }
+
+    bool CanSeePlayer()
+    {
+        //rayDirection = playerObject.transform.position - transform.position;
+        if (Physics.Raycast(transform.position, anchor.transform.position - this.transform.position, myMask))
+        {
+            Debug.Log("Can see player");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Can't see player");
+            return false;
+        }
     }
 }
